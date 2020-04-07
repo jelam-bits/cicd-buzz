318082c3e63dbd01be80fc0e8a1ad6111d0fa946
Jonathan Elam <elam.jonathan@gmail.com>
2020-04-07 14:57:47 -0500

    updated several files

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -7,12 +7,3 @@ else
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
-
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t jelambits/cicd-buzz:$TAG .
-docker push jelambits/cicd-buzz:$TAG
